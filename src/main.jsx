761fef32f2bcfc8d09a6f43e0a92146cce60f108
llauricella <l.lauricella@correo.unimet.edu.ve>
2025-03-09 15:38:51 -0400

    intento de commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Failed to find the root element');
+}
