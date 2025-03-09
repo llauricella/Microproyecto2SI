a17fd0223583c08182a7ce41cf759c99f175bf2d
llauricella <l.lauricella@correo.unimet.edu.ve>
2025-03-09 19:11:28 -0400

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Header from "./components/Header"
-import Principal from "./components/Principal";
-import Login from "./components/Login";
-
-function App() {
-  return <div className="block bg-[url(https://t4.ftcdn.net/jpg/03/98/56/25/360_F_398562516_KRzXMRVHk6I7SUbbWhuWjggmykE6oYQy.jpg)] bg-cover"><Header/><Login/></div>
-}
-
-export default App;
